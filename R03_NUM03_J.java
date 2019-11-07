07c2793d4408943ebb4b797b3e89ee4760a2444a
MbarakatUCF <57231144+MbarakatUCF@users.noreply.github.com>
2019-11-07 03:21:13 -0500

    Update R03_NUM03_J.java
    *R03_NUM03_J
    *Rule 03. Numeric Types and Operations (NUM)
    *NUM03-J. Use integer types that can fully represent the possible range of unsigned data

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -5,7 +5,7 @@
 *NUM03-J. Use integer types that can fully represent the possible range of unsigned data
 *  Compilation:  javac R03_NUM03_J.java 
 *  Execution:    java R03_NUM03_J
-*  reading integer data without considering the signedness of the source
+*  reading integer data with considering the signedness of the source
 * ******************************************************************************/
 import java.io.*;
 
@@ -15,8 +15,8 @@ DataInputStream is = new DataInputStream(System.in);
 System.out.println("Integer = " + getInteger(is));
 }
 
-public static int getInteger(DataInputStream is) throws IOException {
+public static long getInteger(DataInputStream is) throws IOException {
 	System.out.println("Input integer data");
-	return is.readInt();
+	return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
 }
