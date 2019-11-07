9aeb71112f102d9a93ad9e7297fb9a8256138f28
MbarakatUCF <57231144+MbarakatUCF@users.noreply.github.com>
2019-11-07 03:24:41 -0500

    Update R13_FIO14_J.java
    *R13_FIO14_J
    *Rule 13. Input Output (FIO)
    *FIO14-J. Perform proper cleanup at program termination

--- R13_FIO14_J.java
+++ R13_FIO14_J.java
@@ -4,20 +4,26 @@
 *FIO14-J. Perform proper cleanup at program termination
 *  Compilation:  javacR13_FIO14_J.java 
 *  Execution:    java R13_FIO14_J
-* This program closes open files when it exits. The file is closed without the text actually being written.
+* Adds a shutdown hook to close the file. This hook is invoked by Runtime.exit() and is called before the JVM is halted.
 * ******************************************************************************/
 import java.io.*;
 import java.io.PrintStream;
  import java.io.BufferedOutputStream;
  import java.io.IOException;
 
+
 public class R13_FIO14_J {
 
-  public  static void main(String[] args) throws FileNotFoundException {
-    final PrintStream out =
-     new PrintStream(new BufferedOutputStream(new FileOutputStream("foo.txt")));
+  public static void main(String[] args)
+    throws FileNotFoundException {
+    final PrintStream out = 
+      new PrintStream(new BufferedOutputStream(new FileOutputStream("foo.txt")));
+    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() 
+      {   public void run() {
+          out.close();
+      }
+    }));
     out.println("hello");
     Runtime.getRuntime().exit(1);
   }
 }
-
