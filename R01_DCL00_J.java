02eb916804a46188720a10e66111de9fcd37b3d5
MbarakatUCF <57231144+MbarakatUCF@users.noreply.github.com>
2019-11-07 03:19:09 -0500

    Update R01_DCL00_J.java
    *R01_DCL00_J
    *Rule 01. Declarations and Initialization (DCL)
    *DCL00-J. Prevent class initialization cycles

--- R01_DCL00_J.java
+++ R01_DCL00_J.java
@@ -4,7 +4,7 @@
 *DCL00-J. Prevent class initialization cycles
 *  Compilation:  javac R01_DCL00_J.java 
 *  Execution:    java R01_DCL00_J 
-*  Checking balance using static final class variable
+*  Checking balance using static final class variable with correct initalization
 * ******************************************************************************/
 
 
@@ -16,15 +16,15 @@ public class R01_DCL00_J {
 
 	public static class Cycle {
   		private final int balance;
- 		private static final Cycle c = new Cycle();
- 		private static final int deposit = (int) (Math.random() * 100); // Random deposit
- 
+  		private static final int deposit = (int) (Math.random() * 100); // Random deposit
+ 		private static final Cycle c = new Cycle();  // Inserted after initialization of required fields
+  
   		public Cycle() {
     	balance = deposit - 10; // Subtract processing fee
-  		}
+ 		}
  
  		public static void checking(String[] args) {
-    	System.out.println("The account balance is: " + c.balance);
+   		System.out.println("The account balance is: " + c.balance);
   		}
   
 	}
