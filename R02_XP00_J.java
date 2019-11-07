eded50f763a5395cfd28fd2f3b1e557876274d97
MbarakatUCF <57231144+MbarakatUCF@users.noreply.github.com>
2019-11-07 03:20:00 -0500

    Update R02_XP00_J.java
    *R02_XP00_J
    *Rule 02. Expressions (EXP)
    *XP00-J.  Do not ignore values returned by methods

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -4,23 +4,32 @@
 *XP00-J.  Do not ignore values returned by methods
 *  Compilation:  javac R02_XP00_J.java 
 *  Execution:    java R02_XP00_J 
-*  Creating and deleting file
+*  Creating and deleting file and checking for success
 * ******************************************************************************/
-import java.io.File;
-import java.io.IOException;
-
+import java.io.*; 
 
 public class R02_XP00_J {
-
 public static void main(String args[]) {
 deleteFile();
+
 }
 
 public static void deleteFile() {
 	File someFile = new File("hello.txt");
 	//Do something with someFile
 	System.out.println(someFile + " created file object");
-	someFile.delete();
+	
+	if (!someFile.delete()) {
+		//handle failure to delete file
+	System.out.println(someFile + " NOT Deleted");
+}
+else
+{
 	System.out.println(someFile + " Deleted");
 }
 }
+
+}
+
+
+
